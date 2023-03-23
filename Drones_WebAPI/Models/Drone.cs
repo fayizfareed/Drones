0d46f83836dfaeb2d0e33047dd99e9c361dd2786
Fayiz Fareed <vetriyalan@gmail.com>
2023-03-24 01:57:17 +0530

    Medels Mapped

---
+++ Drones_WebAPI/Models/Drone.cs
@@ -0,0 +1,12 @@
+﻿namespace Drones_WebAPI.Models
+{
+    public class Drone
+    {
+        public long Id { get; set; }
+        public string SerialNumber { get; set; }
+        public string Model { get; set; }
+        public double WeightLimit { get; set; }
+        public double BatteryCapacity { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Drones_WebAPI/Models/Medications.cs
@@ -0,0 +1,12 @@
+﻿namespace Drones_WebAPI.Models
+{
+    public class Medications
+    {
+        public int Id { get; set; }
+        public Drone DroneId { get; set; }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Code { get; set; }
+        public string Image { get; set; }
+    }
+}
